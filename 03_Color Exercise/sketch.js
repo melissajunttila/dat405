4fc9e5c15abb8ab2e946f3dba54994a969762d2c
(s) Melissa Junttila <mejunttila@pm7-0093.uopnet.plymouth.ac.uk>
2018-10-23 17:39:08 +0100

    update

--- 03_Color Exercise/sketch.js
+++ 03_Color Exercise/sketch.js
@@ -1,16 +1,19 @@
 function setup() {
   // Create the canvas
   createCanvas(600, 500);
-  background(0);
+  background(51);
 
   // Set colors
   fill(255, 255, 255, 255);
   stroke(130, 101, 127);
 
   // An ellipse
-  ellipse(50, 50, 100, 100);
-  ellipse(550, 450, 100, 100);
-  ellipse(50, 450, 100, 100);
-  ellipse(50, 550, 100, 100);
+ellipse(50, 50, 100, 100);
+ellipse(550, 450, 100, 100);
+ellipse(50, 450, 100, 100);
+ellipse(550, 50, 100, 100);
+line(0, 0, 600, 500)
+line(50, 0, 50, 0)
+
 
 }
