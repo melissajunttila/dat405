9da7510e85fcd696f42a5f6ded0b38513fec85cf
melissajunttila <44362037+melissajunttila@users.noreply.github.com>
2018-11-05 12:05:54 +0000

    Add files via upload

---
+++ For loops practice unrolled/sketch.js
@@ -0,0 +1,29 @@
+let ypos = 200;
+let xpos = 80;
+let xstep = 30;
+
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background(255);
+  fill(50);
+  noStroke();
+
+    ellipse(xpos + (xstep * 0), ypos, 20, 20);
+    ellipse(xpos + (xstep * 1), ypos, 20, 20);
+    ellipse(xpos + (xstep * 2), ypos, 20, 20);
+    ellipse(xpos + (xstep * 3), ypos, 20, 20);
+    ellipse(xpos + (xstep * 4), ypos, 20, 20);
+    ellipse(xpos + (xstep * 5), ypos, 20, 20);
+    ellipse(xpos + (xstep * 6), ypos, 20, 20);
+    ellipse(xpos + (xstep * 7), ypos, 20, 20);
+    ellipse(xpos + (xstep * 8), ypos, 20, 20);
+    ellipse(xpos + (xstep * 9), ypos, 20, 20);
+    ellipse(xpos + (xstep * 10), ypos, 20, 20);
+    ellipse(xpos + (xstep * 11), ypos, 20, 20);
+    ellipse(xpos + (xstep * 12), ypos, 20, 20);
+
+  }
