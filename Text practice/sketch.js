e3179848ea0577ea8ab9f78e481b24d6f63ecc3e
melissajunttila <44362037+melissajunttila@users.noreply.github.com>
2018-11-19 12:15:48 +0000

    update

---
+++ Text practice/sketch.js
@@ -0,0 +1,16 @@
+function setup () {
+  createCanvas(600, 600);
+}
+
+function draw() {
+background(200);
+  fill(0, 0, 0);
+  textSize(40);
+  textFont('Courier New');
+  text("trying my best", 100, 200);
+  fill(204, 51, 255);
+  text("to learn", 100, 300);
+  fill(255, 0, 255);
+  text("creative coding", 100, 400);
+
+}
