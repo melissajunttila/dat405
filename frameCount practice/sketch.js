b674c662ca5149e46d38d56a1daf86ae97a37a57
melissajunttila <44362037+melissajunttila@users.noreply.github.com>
2018-11-25 13:21:46 +0000

    Add files via upload

---
+++ frameCount practice/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background(128, 0, 64);
+}
+
+function draw() {
+  fill(255,255,255)
+  textSize(80);
+  textFont('Courier New');
+  text("where are we", frameCount, 100, 100);
+}
